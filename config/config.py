012f17ee9c4b2162155666f8c7e4f2f8c5015c10
Zevobla <ghost-brake-your-home@protonmail.com>
2024-11-15 22:26:14 +0300

    Reorganize configuration structure to docker-compose.yml

--- config/config.py
+++
@@ -1,7 +0,0 @@
-import tomllib
-from pathlib import Path
-
-class Config:
-    def __init__(self, file: Path = "config/config.toml") -> None:
-        with open(file, "rb") as f:
-            self.conf = tomllib.load(f)
