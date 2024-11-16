2b11463cbd8b030474e0f17caea62fd078d63da4
Zevobla <ghost-brake-your-home@protonmail.com>
2024-11-16 10:59:34 +0300

    Port already in use

--- queue/main.py
+++ queue/main.py
@@ -1,4 +1,6 @@
 """This module helps handling aac stream."""
+import os
+
 from fastapi import FastAPI
 import uvicorn
 
@@ -26,4 +28,4 @@ def add_track(track_id: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=os.environ["SERVICE_PORT"], reload=True)
--- spotify/main.py
+++ spotify/main.py
@@ -29,4 +29,4 @@ def get_track(track_id: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=os.environ["SERVICE_PORT"], reload=True)
